97d48f44fd92c8d2f1f68c1fca52086592947cbb
katsuraku <kirsten.jones@ft.com>
2017-05-01 22:26:42 +0100

    Emulate fetching data from an api instead of hardcoded data in file

--- schema/schema.js
+++ schema/schema.js
@@ -1,26 +1,10 @@
-// Schema tells graphQL exactly what app data looks like
-// - what properties each object has
-// - how each object is realted to each other object
-
 const graphql = require('graphql');
-const _ = require('lodash');
-
-// user has a first name, reference to a company and a position
-// company has a name
-// position has a name
-// users are related to other users (some kind of friendship)
-
-// hardcode some data to begin with
-const users = [
-  { id: "23", firstName: "Bill", age: 20 },
-  { id: "47", firstName: "Samantha", age: 40 }
-];
-
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema // helper - takes in a rootquery, returns a graphQL schema instance
+  GraphQLSchema
 } = graphql;
 
 const UserType = new GraphQLObjectType({
@@ -32,22 +16,17 @@ const UserType = new GraphQLObjectType({
   }
 });
 
-// RootQuery is to let GraphQL jump into a specific node in the data
-// Fields - sets up how to query specific nodes, showing what args are to be provided. 'type' is the shape of the data it should return. 'args' is what args the query should come with to signpost the specific node. 'resolve' is where the actual data is found and returned.
-// ask me about users if you give ma the id (in the args) I will return you the UserType (in the type)
-// resolve: go into DB and find the actual data.
-// args inside the resolve function: specifies what actual data will be returned - here referring to 'args' means the id will get returned.
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     user: {
       type: UserType,
-      // tell the rootquery to expect to receive an id as an argument (i.e. you'll provide an id as the info used to jump to the specific node )
       args: { id: { type: GraphQLString } },
-      // make it so the args (here just the id) will be available in the return data when the resolve function executes. i.e. when actual data is fetched it's the id that will be fetched and returned.
       resolve(parentValue, args) {
-        // args will be an object that includes an id property
-        return _.find(users, { id: args.id }); // find the first user who has an id equal to args.id
+        // make resolve return a promise to work asynchronously.
+        // graphql detects that it is a promise and waits for the promise to resolve, then returns the data it resolves with to the client
+        return axios.get(`http://localhost:3000/users/${args.id}`) // like fetch
+          .then(res => res.data); // quirk of axios - when the promise resolves it comes back in this shape { data: returnedData }
       }
     }
   }
